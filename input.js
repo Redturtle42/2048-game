084947c8385a4be41d01812e9642161a56eb1150
Redturtle <izabella.tanacs@yahoo.com>
2020-05-26 12:38:36 +0200

    shorten input.js and merge to development branch

--- input.js
+++ input.js
@@ -10,34 +10,22 @@ const gameBoard = map.generateMatrix(4, 4);
 
 termkit.grabInput();
 termkit.on('key', function (key) {
+  termkit.clear();
   if (key === 'UP') {
-    termkit.clear();
     move.up(gameBoard);
-    console.log(table.table(gameBoard));
-    console.log('U press up cursor.');
   } else if (key === 'DOWN') {
-    termkit.clear();
     move.down(gameBoard);
-    console.log(table.table(gameBoard));
-    console.log('U press down cursor.');
   } else if (key === 'RIGHT') {
-    termkit.clear();
     move.right(gameBoard);
-    console.log(table.table(gameBoard));
-    console.log('U press right cursor.');
   } else if (key === 'LEFT') {
-    termkit.clear();
     move.left(gameBoard);
-    console.log(table.table(gameBoard));
-    console.log('U press left cursor.');
   } else if (key === 'ENTER') {
-    console.log('U press enter.');
   } else if (key === 'CTRL_C' || key === 'q' || key === 'ESCAPE') {
     console.log('Goodbye!');
     process.exit();
-  } else {
-    console.log('You press', key, 'key');
   }
+  move.genNewElements(gameBoard);
+  console.log(table.table(gameBoard));
 });
 
 module.exports = {
