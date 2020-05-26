29d3f0f2f3076f38c7ad7ba848fbf838a6f2da83
Redturtle <izabella.tanacs@yahoo.com>
2020-05-26 12:23:26 +0200

    index.js merge to development banch

--- index.js
+++ index.js
@@ -5,6 +5,9 @@
 const table = require('table');
 const inputJs = require('./input');
 const mapJs = require('./map');
+const checkJs = require('./check');
+// const moveJs = require('./move');
 
 mapJs.fillMatrix(inputJs.gameBoard);
 console.log(table.table(inputJs.gameBoard));
+checkJs.checkLastMove(inputJs.gameBoard);
